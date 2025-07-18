100a86ae28ef8ad3d8585ce100ab7f3ddd866423
gift-ndoma <giftndoma87@gmail.com>
2025-07-18 17:53:45 +0100

    last commit

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,9 +1,9 @@
 import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 export default class ProductDetails {
+  
   constructor(productId, dataSource) {
     this.productId = productId;
-
     this.product = {};
     this.dataSource = dataSource;
   }
