a304b0ae4aa376d364de18e269301b6adb74dcd4
gift-ndoma <giftndoma87@gmail.com>
2025-07-25 22:54:01 +0100

    added email newsletter

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,21 @@
 
 
 import { loadHeaderFooter } from "./utils.mjs"; // Import loadHeaderFooter
-import Alert from "/js/Alert.js";
+import Alert from "../js/Alert.js";
 
 loadHeaderFooter(); // Call this function to dynamically load the header and footer, and update the cart count on page load.
 new Alert("../json/Alert.json");
 //moved the product-listing code outside of main.js to product-listing.js
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("newsletter-form");
+  if (form) {
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      alert("Thank you for subscribing to our newsletter!");
+      form.reset();
+    });
+  }
+});
 
 
