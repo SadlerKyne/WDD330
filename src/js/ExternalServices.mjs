c42c623de65013144a03e71385e6a8ff8cbafb1c
Sam <sammybonfanti@gmail.com>
2025-07-29 00:14:23 -0700

    Refactor ExternalServices to improve error handling and streamline JSON conversion

--- src/js/ExternalServices.mjs
+++ src/js/ExternalServices.mjs
@@ -1,42 +1,39 @@
 const baseURL = import.meta.env.VITE_SERVER_URL;
 
-async function convertToJson(res) {
-  const jsonResponse = await res.json(); //  try to parse JSON
+function convertToJson(res) {
   if (res.ok) {
-    return jsonResponse;
+    return res.json();
   } else {
-
-    throw { name: "servicesError", message: jsonResponse }; // Include jsonResponse in the error message 
+    throw new Error("Bad Response");
   }
 }
 
 export default class ExternalServices {
-  constructor() {}
-
+  constructor() {
+    // this.category = category;
+    // this.path = `../public/json/${this.category}.json`;
+  }
   async getData(category) {
-    const url = `${baseURL}products/search/${category}`;
-    console.log(`Fetching from: ${url}`);
-    const response = await fetch(url);
+    const response = await fetch(`${baseURL}products/search/${category}`);
     const data = await convertToJson(response);
+
     return data.Result;
   }
-
   async findProductById(id) {
     const response = await fetch(`${baseURL}product/${id}`);
     const data = await convertToJson(response);
+    // console.log(data.Result);
     return data.Result;
   }
 
   async checkout(payload) {
     const options = {
-      method: "POST", 
+      method: "POST",
       headers: {
-        "Content-Type": "application/json", // Set content type to JSON 
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(payload), 
+      body: JSON.stringify(payload),
     };
-    const response = await fetch(`${baseURL}checkout`, options); // Send POST to checkout endpoint 
-    const data = await convertToJson(response); // Process the response
-    return data;
+    return await fetch(`${baseURL}checkout/`, options).then(convertToJson);
   }
 }
